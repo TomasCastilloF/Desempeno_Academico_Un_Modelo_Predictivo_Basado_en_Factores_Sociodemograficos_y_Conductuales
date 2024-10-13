628380a41907787594ccfaa0d9255762c365b318
Tomás Castillo <82657626+TomasCastilloF@users.noreply.github.com>
2024-10-13 17:36:41 -0300

    modificación de código erroneo

--- func_object_to_int.py
+++ func_object_to_int.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMXBMGhxX619HbT+DGUctAH",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
